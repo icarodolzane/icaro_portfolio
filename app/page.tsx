76b92e4c76e5da91241c14fd9f79b2aa74659cda
Icaro <icarodolzane@live.com>
2023-08-03 20:20:23 -0400

    feat: section conhecimentos feita.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import { HeroSection } from "./components/pages/home/hero-section";
+import { HeroSection } from "./components/pages/home";
+import { KnownTechs } from "./components/pages/known-techs";
 
 export default async function Home() {
   return (
     <>
       <HeroSection/>
+      <KnownTechs/>
     </>
   )
 }
