1fdae6f6be6eef25cf5eebb957ed8c526c16e8b1
kamui-Hebertt <heberttaferreira.com@gmail.com>
2023-09-08 03:18:37 -0300

    style:rounded horizon divider

--- app/components/divider/horizontal.tsx
+++ app/components/divider/horizontal.tsx
@@ -11,8 +11,8 @@ export const HorizontalDivider = ({className}: HorizontalDividerProps) => {
           "w-1/2", 
           "h-1.5 sm:h-2", 
           "bg-gradient-to-r from-blue-400 via-blue-500 to-purple-700",
-          "my-8 border-b",
-          "ml-[-2rem] sm:ml-[-7rem]", 
+          "my-8 border-b rounded-r-full",
+          "ml-[-2rem] sm:ml-[-7rem]",
           className
         )
       }
