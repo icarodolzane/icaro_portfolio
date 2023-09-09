d7c7c464f58b28ad8a7bec71781c85a9b8b06ba6
kamui-Hebertt <heberttaferreira.com@gmail.com>
2023-09-09 05:21:40 -0300

    feat: stacks

--- app/components/pages/home/known-techs/index.tsx
+++ app/components/pages/home/known-techs/index.tsx
@@ -1,32 +1,49 @@
-'use client'
-import { SectionTitle } from "@/app/components/section-title"
-import { KnownTech as IKnownTech} from "@/app/types/projects"
-import { KnownTech } from "./known-tech"
-import { motion } from "framer-motion"
-import { HorizontalDivider } from "@/app/components/divider/horizontal" 
+"use client";
+import { SectionTitle } from "@/app/components/section-title";
+import { KnownTech as IKnownTech } from "@/app/types/projects";
+import { KnownTech } from "./known-tech";
+import { motion } from "framer-motion";
+import { HorizontalDivider } from "@/app/components/divider/horizontal";
 type KnownTechProps = {
-  techs: IKnownTech[]
-}
+  techs: IKnownTech[];
+};
 
-export const KnownTechs = ({techs}: KnownTechProps) => {
+export const KnownTechs = ({ techs }: KnownTechProps) => {
   return (
-    <section id="skills" className="container py-16">
-      <HorizontalDivider />
-      <SectionTitle title="Conhecimentos" subtitle="competências"/>
-      <div className="w-full grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-[60px]">
-        {
-          techs?.map((tech, i) => (
+    <>
+      <section id="skills" className=" overflow-x-hidden">
+        <HorizontalDivider />
+        <SectionTitle
+          title="Conhecimentos"
+          subtitle="competências"
+          className="ml-6 sm:ml-32"
+        />
+        <div className="w-full sm:ml-[-7.5rem] sm:w-[100rem] lg:w-[100rem] grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] ">
+          {techs?.slice(0, 3).map((tech, i) => (
             <motion.div
-              initial={{opacity: 0, x: -100}}
-              whileInView={{opacity: 1, x: 0}}
-              exit={{opacity: 0, x: -100}}
-              transition={{duration: 0.15, delay: i * 0.1}}
+              initial={{ opacity: 0, x: -100 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: -100 }}
+              transition={{ duration: 0.10, delay: i * 0.1 }}
             >
-              <KnownTech key={tech.name} tech={tech}/>
+              <KnownTech key={tech.name} tech={tech} />
             </motion.div>
-          ))
-        }
-      </div>
-    </section>
-  )
-}
+          ))}
+        </div>
+
+        <div className=" w-full  sm:ml-[-7.5rem] sm:w-[100rem] grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))]">
+          {techs?.slice(3, 99).map((tech, i) => (
+            <motion.div
+              initial={{ opacity: 0, x: -100 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: -100 }}
+              transition={{ duration: 0.15, delay: i * 0.1 }}
+            >
+              <KnownTech key={tech.name} tech={tech} />
+            </motion.div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
--- app/components/pages/home/known-techs/known-tech.tsx
+++ app/components/pages/home/known-techs/known-tech.tsx
@@ -8,12 +8,14 @@ type KnownTechProps = {
 export const KnownTech = ({tech}: KnownTechProps) => {
   const relativeTime = getRelativeTimeString(new Date(tech.startDate), "pt-BR")?.replace("há ", "")
   return (
-    <div className="p-6 rounded-lg bg-gray-600/20 text-gray-500 flex flex-col gap-2 hover:text-emerald-500 hover:bg-gray-600/30 transition-all">
-      <div className="flex items-center justify-between">
-        <p className="font-medium">{tech.name}</p>
+<section id="skills" className=" container py-5 relative ">
+
+<div className=" text-white flex items-center justify-center flex-col bg-[#7E41CA] h-40 "  style={{ transform: 'skewX(-27deg)' }}>
+    <p className="font-medium">{tech.name}</p>
         <CMSIcon icon={tech.iconSvg}/>
+      <span>{relativeTime} de experiência.</span> 
       </div>
-      <span>{relativeTime} de experiência.</span>
-    </div>
+
+</section>
   )
 }
